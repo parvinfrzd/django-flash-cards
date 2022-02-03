79af7534ed1c51102ce50a14cedd0783c04d1439
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-03 12:05:13 -0500

    modify and alter models

--- django_flashcards/flashcards/models.py
+++ django_flashcards/flashcards/models.py
@@ -6,12 +6,14 @@ from django.contrib.auth.models import User
 class Category(models.Model): 
     name = models.CharField(max_length=50)
     
+    def __str__(self):
+        return self.name
     
 class FlashCard(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     front = models.TextField()
     back = models.TextField()
-    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    creator = models.CharField(max_length=50)
     is_known = models.BooleanField(default=False)
     pub_date = models.DateTimeField(default=timezone.now())
     
