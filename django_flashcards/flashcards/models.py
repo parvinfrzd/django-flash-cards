e0e033c77fa1a35faf86483bd94b281c48c55179
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-03 14:41:30 -0500

    generate absolute path in models

--- django_flashcards/flashcards/models.py
+++ django_flashcards/flashcards/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Category(models.Model): 
@@ -19,3 +20,7 @@ class FlashCard(models.Model):
     
     def __str__(self):
         return self.front
+    
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"flashcard_id": self.pk})
+    
