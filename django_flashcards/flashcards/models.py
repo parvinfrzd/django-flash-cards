9c0de4b428e55a6854956cf5e860ee90d360ce92
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-03 11:07:42 -0500

    add model classes

--- django_flashcards/flashcards/models.py
+++ django_flashcards/flashcards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model): 
+    name = models.CharField(max_length=50)
+    
+    
+class FlashCard(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    front = models.TextField()
+    back = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_known = models.BooleanField(default=False)
+    pub_date = models.DateTimeField(default=timezone.now())
+    
+    def __str__(self):
+        return self.front
