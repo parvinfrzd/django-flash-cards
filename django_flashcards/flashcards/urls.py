b3cc9dd84d94c25e9af4ed153b76972c44c9ce89
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-03 13:20:21 -0500

    update index page with models

--- django_flashcards/flashcards/urls.py
+++ django_flashcards/flashcards/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'), 
     path('about/',views.about, name='about'),
     path('flashcards/', views.index, name='index'),
+    path('flashcards/<int:flashcard_id>', views.detail, name='detail'),
     path('flashcards/new/', views.new, name='new'),
 
 ]
--- django_flashcards/flashcards/views.py
+++ django_flashcards/flashcards/views.py
@@ -1,29 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-class Flashcard:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, category, front, back, creator, likes, known):
-    self.category = category
-    self.front = front
-    self.back = back
-    self.creator = creator
-    self.likes = likes 
-    self.known = known
-
-data = [
-  Flashcard('Arrays',
-            'What is an array?',
-            'An array is a collection of similar data elements stored at contiguous memory locations.',
-            'Parvin',0,False),
-  Flashcard('Big O Notation', 
-            'What is Big O?  ',
-            'is written in the form of O(n) where O stands for “order of magnitude” and n represents what we\'re comparing the complexity of a task against.',
-            'Parvin',4,False),
-  Flashcard('Arrays',
-            'What is linked list?',
-            'A linked list is a linear data structure, in which the elements are not stored at contiguous memory locations.',
-            'Parvin',2,True)
-]
+from .models import Category, FlashCard
 
 # Create your views here.
 def home (request): 
@@ -33,4 +10,12 @@ def about(request):
     return render(request, 'flashcards/about.html')
 
 def index(request): 
-    return render (request, 'flashcards/index.html', { 'data': data })
+    flashcards = FlashCard.objects.all()
+    return render (request, 'flashcards/index.html', { 'flashcards': flashcards })
+  
+def detail(request,flashcard_id):
+      flashcard = FlashCard.objects.get(id=flashcard_id)
+      return render(request, 'flashcards/detail.html', {'flashcard': flashcard})
+  
+def new(request): 
+  return render (request, 'flashcards/new.html')
