cda59164b4af9e4946ac951960fd2f49e193c54c
Parvin Farahzadeh <parv93farah@gmail.com>
2022-02-04 11:05:55 -0500

    add delete and update views

--- django_flashcards/flashcards/views.py
+++ django_flashcards/flashcards/views.py
@@ -1,7 +1,10 @@
 from dataclasses import field
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.forms import ModelForm
+
+
 from .models import Category, FlashCard
 
 # Create your views here.
@@ -20,6 +23,34 @@ def detail(request,flashcard_id):
       print(flashcard.id)
       return render(request, 'flashcards/detail.html', {'flashcard': flashcard})
   
+class CategoryCreate(CreateView):
+    model = Category 
+    fields = '__all__'
+    
+# class AddBookForm(ModelForm):
+#     class Meta:
+#         model = Book
+#         fields = ('title', 'person',)
+#         widgets = {
+#             'person': forms.HiddenInput,
+#         } 
+          
 class FlashcardCreate(CreateView): 
     model = FlashCard
     fields = ['front','back','creator','category']
+    
+    def form_valid(self,form): 
+        form.instance.category_id = self.kwargs.get('pk')
+        return super(FlashcardCreate, self.form_valid(form))
+ 
+class FlashcardUpdate(UpdateView): 
+    model = FlashCard
+    fields = ['front','back']
+    
+class FlashcardDelete(DeleteView):
+    model = FlashCard
+    fields = '__all__'
+    success_url = '/flashcards/'
+
+
+    
